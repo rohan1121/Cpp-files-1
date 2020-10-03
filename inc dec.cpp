c3fdb289130d733b14cc20cdd9ffe0795dc5a17c
rohan1121 <72290421+rohan1121@users.noreply.github.com>
2020-10-03 12:40:21 +0530

    checking increment of decrement source code

---
+++ inc dec.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	if(a%4==0){
+	    a++;
+	}
+	else
+	{
+	    a--;
+	}
+	cout<<a;
+	return 0;
+}
